88e363c160c673aa0fa6da8de2354eed5afde982
Dimitris Damilos <d.k.damilos@gmail.com>
2021-03-06 14:52:52 +0000

    Add all DnD

--- src/index.js
+++ src/index.js
@@ -1,17 +1,38 @@
 import React, { StrictMode } from "react";
 import ReactDOM from "react-dom";
 import { Provider } from "react-redux";
-import { DndProvider } from "react-dnd";
 import { HTML5Backend } from "react-dnd-html5-backend";
-
+import { TouchBackend } from "react-dnd-touch-backend";
+import {
+  DndProvider,
+  TouchTransition,
+  MouseTransition,
+} from "react-dnd-multi-backend";
 import store from "./redux";
 import App from "./components/App";
 import "./index.css";
 
+const HTML5toTouch = {
+  backends: [
+    {
+      id: "html5",
+      backend: HTML5Backend,
+      transition: MouseTransition,
+    },
+    {
+      id: "touch",
+      backend: TouchBackend,
+      options: { enableMouseEvents: true },
+      preview: true,
+      transition: TouchTransition,
+    },
+  ],
+};
+
 ReactDOM.render(
   <StrictMode>
     <Provider store={store}>
-      <DndProvider backend={HTML5Backend}>
+      <DndProvider options={HTML5toTouch}>
         <App />
       </DndProvider>
     </Provider>
